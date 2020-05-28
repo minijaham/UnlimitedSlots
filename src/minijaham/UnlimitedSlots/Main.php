f4020a6d58d484442751d32260253d1c5cbaa915
minijaham <60860894+minijaham@users.noreply.github.com>
2020-05-28 12:50:15 -0700

    Update Main.php

--- src/minijaham/UnlimitedSlots/Main.php
+++ src/minijaham/UnlimitedSlots/Main.php
@@ -6,14 +6,11 @@ use pocketmine\event\Listener;
 use pocketmine\event\server\QueryRegenerateEvent;
 use pocketmine\plugin\PluginBase;
 
-
 class Main extends PluginBase implements Listener {
-
 
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents($this,$this);
     }
-
     public function onQuery(QueryRegenerateEvent $event) {
         $event->setMaxPlayerCount(intval(count($this->getServer()->getOnlinePlayers())+1));
     }
